62478109d6495f8e66fa8e9f38ab164c13302514
Brandon <brandon999880@gmail.com>
2020-02-05 18:11:34 +1300

    Updates

---
+++ precache-manifest.c393a8a64eb2218d76c86dda313fe31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca8257679f02755cc3763f446da1531",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "55d00caf4bf89cea6a99",
+    "url": "/sudoku/static/css/main.fdd051a6.chunk.css"
+  },
+  {
+    "revision": "8fc2b64ae6439e9d1e3a",
+    "url": "/sudoku/static/js/2.3ee41fd8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sudoku/static/js/2.3ee41fd8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55d00caf4bf89cea6a99",
+    "url": "/sudoku/static/js/main.6f869e04.chunk.js"
+  },
+  {
+    "revision": "80fdc32de6d895578ae2",
+    "url": "/sudoku/static/js/runtime-main.c8e770ab.js"
+  }
+]);
